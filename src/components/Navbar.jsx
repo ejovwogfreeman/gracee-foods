63857cdc60c9f6b8ec006931538566a0bad4c436
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-09-02 00:09:18 +0100

    message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
-import "../css/Navbar.css"
-import MuiDrawer from './MuiDrawer'
-import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../css/Navbar.css";
+import MuiDrawer from "./MuiDrawer";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-        <div className='logo'>
-            <Link to='/'> <img src={logo} alt=""/> <h2>Lilies</h2></Link>
-        </div>
-        <div className="links">
-            <Link to="/" style={{marginRight: '10px'}}>Home</Link>
-            <Link to="/signin" style={{marginRight: '10px'}}>Login</Link>
-            <Link to="/signup" style={{marginRight: '10px', backgroundColor: '#E2B887', color: 'black'}}>Sign Up</Link>
-        </div>
-        <div className='mui-drawer'>
-            <MuiDrawer/>
-        </div>
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="" />
+          <span>
+            <h2>GRACEE</h2>
+            <span>foods</span>
+          </span>
+        </Link>
+      </div>
+      <div className="links">
+        <Link to="/" style={{ marginRight: "10px" }}>
+          Home
+        </Link>
+        <Link to="/signin" style={{ marginRight: "10px" }}>
+          Login
+        </Link>
+        <Link
+          to="/signup"
+          style={{
+            marginRight: "10px",
+            backgroundColor: "#E2B887",
+            color: "black",
+          }}
+        >
+          Sign Up
+        </Link>
+      </div>
+      <div className="mui-drawer">
+        <MuiDrawer />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
